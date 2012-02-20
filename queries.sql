e5a4d39f85056421cbcabe70e1ee6f128571c3b2
Dr. Tristan Allwood <tora@doc.ic.ac.uk>
2012-02-20 09:21:27 +0000

    Initial import

---
+++ queries.sql
@@ -0,0 +1,19 @@
+/* q1, columns: title, directory */
+
+/* q2, columns: title, length, made */
+
+/* q3, columns: title, director, made, length */
+
+/* q4, columns: name */
+
+/* q5, columns: name, part, title */
+
+/* q6, columns: name, part, title */
+
+/* q7, columns: name, part, title */
+
+/* q8, columns: count */
+
+/* q9, columns: name, title, director */
+
+/* q10, columns: name */
